3a82b12c3f27ccef0b766e872193fd67082dfc94
ambikaGangwar <74405214+ambikaGangwar@users.noreply.github.com>
2021-03-21 15:03:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var canvas, backgroundImage;
+ var canvas, backgroundImage;
 
 var gameState = 0;
 var contestantCount;
@@ -10,7 +10,7 @@ var question, contestant, quiz;
 
 
 function setup(){
-  canvas = createCanvas(850,900);
+  canvas = createCanvas(850,400);
   database = firebase.database();
   quiz = new Quiz();
   quiz.getState();
